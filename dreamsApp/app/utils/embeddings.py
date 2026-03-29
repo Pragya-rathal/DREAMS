41c4f9fdf43ad7339992f6cd10f5da2506ace546
Pragya-rathal <pragya.rathal@gmail.com>
2026-03-29 22:19:52 +0530

    Update embeddings.py

--- dreamsApp/app/utils/embeddings.py
+++ dreamsApp/app/utils/embeddings.py
@@ -3,6 +3,6 @@ import numpy as np
 
 def l2_normalize(vec: np.ndarray) -> np.ndarray:
     norm = np.linalg.norm(vec)
-    if norm == 0:
+  if np.isclose(norm, 0):
         return vec.copy()
     return vec / norm
